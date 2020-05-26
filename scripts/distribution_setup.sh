1d56954527b3959dddb7f4f909298c2ce915f260
Andrew Kaplanov <akaplanov@gmail.com>
2020-05-26 14:58:16 +0500

    .

--- scripts/distribution_setup.sh
+++ scripts/distribution_setup.sh
@@ -32,7 +32,7 @@ if [ "$1" == "developer" ]; then
   port=$2
   cat << EOF > distribution.json
 {
-  "port" : "${port}"
+  "port" : ${port}
 }
 EOF
   cat << EOF > distribution_pm2.json
@@ -60,7 +60,7 @@ elif [ "$1" == "client" ]; then
   distribDirectoryUrl=$3
   cat << EOF > distribution.json
 {
-  "port" : "${port}",
+  "port" : ${port},
   "developerDistributionUrl" : "${distribDirectoryUrl}"
 }
 EOF
