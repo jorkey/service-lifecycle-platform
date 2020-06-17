db8a2829acc56eb3f09670e48beb3454976272c7
Andrew Kaplanov <akaplanov@gmail.com>
2020-06-17 23:09:26 +0500

    Partly refactored update.sh

--- scripts/update.sh
+++ scripts/update.sh
@@ -12,16 +12,16 @@
 #  $1 - source URL
 #  $2 - output file
 function download {
-  url=$1
-  outputFile=$2
+  local url=$1
+  local outputFile=$2
   rm -f ${outputFile}
   http_code=`curl ${url} --output ${outputFile} --write-out "%{http_code}" --connect-timeout 5 --silent --show-error`
   if [ "$?" != "0" ]; then
     exit 1
   elif [[ "${url}" == http* ]] && [ "$http_code" != "200" ]; then
     if [ -f ${url} ]; then
-      echo "Request: ${url}"
-      echo "Response: `cat ${outputFile}`"
+      >&2 echo "Request: ${url}"
+      >&2 echo "Response: `cat ${outputFile}`"
       rm ${outputFile}
     fi
     exit 1
@@ -34,19 +34,19 @@ function download {
 # output:
 #   stdout - version number
 function getDesiredVersion {
-  service=$1
+  local service=$1
   if [[ ${distribDirectoryUrl} == http://* ]] || [[ ${distribDirectoryUrl} == https://* ]]; then
-    desiredVersionFile=.desired-version-${service}.json
+    local desiredVersionFile=.desired-version-${service}.json
     download ${distribDirectoryUrl}/download-desired-version/${service}?image=false ${desiredVersionFile}
     cat ${desiredVersionFile}
     rm -f ${desiredVersionFile}
   elif [[ ${distribDirectoryUrl} == file://* ]]; then
-    desiredVersionsFile=.desired-versions.json
+    local desiredVersionsFile=.desired-versions.json
     download ${distribDirectoryUrl}/desired-versions.json ${desiredVersionsFile}
     jq -r .desiredVersions.${service} ${desiredVersionsFile}
     rm -f ${desiredVersionsFile}
   else
-    echo "Invalid distribution directory URL ${distribDirectoryUrl}"
+    >&2 echo "Invalid distribution directory URL ${distribDirectoryUrl}"
     exit 1
   fi
 }
@@ -57,17 +57,17 @@ function getDesiredVersion {
 #  $2 - service version
 #  $3 - output file
 function downloadVersionImage {
-  service=$1
-  version=$2
-  outputFile=$3
+  local service=$1
+  local version=$2
+  local outputFile=$3
   if [[ ${distribDirectoryUrl} == http://* ]] || [[ ${distribDirectoryUrl} == https://* ]]; then
-    echo "Download version ${version} image of service ${service}"
+    >&2 echo "Download version ${version} image of service ${service}"
     download ${distribDirectoryUrl}/download-version/${service}/${version} ${outputFile}
   elif [[ ${distribDirectoryUrl} == file://* ]]; then
-    echo "Get version ${version} image of service ${service}"
+    >&2 echo "Get version ${version} image of service ${service}"
     download ${distribDirectoryUrl}/services/${service}/${service}-${version}.zip ${outputFile}
   else
-    echo "Invalid distribution directory URL ${distribDirectoryUrl}"; exit 1
+    >&2 echo "Invalid distribution directory URL ${distribDirectoryUrl}"; exit 1
   fi
 }
 
@@ -76,15 +76,15 @@ function downloadVersionImage {
 #  $@ - external arguments
 #  $serviceToSetup - service to extract script files
 function updateScripts {
-  scriptsZipFile=.scripts.zip
+  local scriptsZipFile=.scripts.zip
   updateService scripts ${scriptsZipFile}
   if [ -f ${scriptsZipFile} ]; then
-    echo "Update scripts"
+    >&2 echo "Update scripts"
     unzip -qo ${scriptsZipFile} update.sh
     unzip -qjo ${scriptsZipFile} ${serviceToSetup}/*
     rm -f ${scriptsZipFile}
     chmod +x *.sh
-    echo "Restart $0"
+    >&2 echo "Restart $0"
     exec $0 "$@"
   fi
 }
@@ -95,9 +95,9 @@ function updateScripts {
 # output:
 #  new version
 function updateJavaService {
-  service=$1
-  serviceZipFile=.${service}.zip
-  newVersion=`updateService ${sevice} ${serviceZipFile}`
+  local service=$1
+  local serviceZipFile=.${service}.zip
+  local newVersion=`updateService ${service} ${serviceZipFile}`
   if [ -f ${serviceZipFile} ]; then
     rm -f ${service}-*.jar
     unzip -qo ${serviceZipFile}
@@ -113,13 +113,13 @@ function updateJavaService {
 # output:
 #  new version
 function updateService {
-  service=$1
-  outputFile=$2
-  echo "Check for new version of ${service}"
-  serviceVersionFile=.${service}.version
-  serviceDesiredVersion=`getDesiredVersion ${service}`
+  local service=$1
+  local outputFile=$2
+  >&2 echo "Check for new version of ${service}"
+  local serviceVersionFile=.${service}.version
+  local serviceDesiredVersion=`getDesiredVersion ${service}`
   if [ ! -f ${serviceVersionFile} ] || [[ "`cat ${serviceVersionFile}`" != "${serviceDesiredVersion}" ]]; then
-    echo "Download service version ${serviceDesiredVersion}"
+    >&2 echo "Download service version ${serviceDesiredVersion}"
     downloadVersionImage ${service} ${serviceDesiredVersion} ${outputFile}
     echo ${serviceDesiredVersion} >${serviceVersionFile}
   fi
@@ -136,14 +136,14 @@ function runService {
     serviceToSetup=${serviceToRun}
     updateScripts "$@"
 
-    buildVersion=`updateJavaService ${serviceToRun} | sed -e 's/_.*//'`
+    local buildVersion=`updateJavaService ${serviceToRun} | sed -e 's/_.*//'`
 
     if [ -f install.json ]; then
       command=`jq -r '.runService.command' install.json`
       args=`jq -r '.runService.args | join(" ")' install.json | sed -e s/%%version%%/${buildVersion}/`
     else
       if [ ! -f ${serviceToRun}-${buildVersion}.jar ]; then
-        echo "No <${serviceToRun}-${buildVersion}>.jar in the build."
+        >&2 echo "No <${serviceToRun}-${buildVersion}>.jar in the build."
         exit 1
       fi
       command="/usr/bin/java"
@@ -154,7 +154,7 @@ function runService {
     ${command} ${args} "$@"
 
     if [ $? -eq 9 ]; then
-      echo "Service ${serviceToRun} is obsoleted. Update it."
+      >&2 echo "Service ${serviceToRun} is obsoleted. Update it."
     else
       break
     fi
@@ -167,6 +167,6 @@ if [ ! -z "${serviceToRun}" ]; then
 elif [ ! -z "${serviceToSetup}" ]; then
   updateScripts "$@"
 else
-  echo "Variable serviceToRun or serviceToSetup must be set"
+  >&2 echo "Variable serviceToRun or serviceToSetup must be set"
   exit 1
 fi
