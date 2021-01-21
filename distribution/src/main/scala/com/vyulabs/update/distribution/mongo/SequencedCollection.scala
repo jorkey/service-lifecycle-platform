fe73059931e73187fefed2fee74eebafd67f2ff7
Andrei Kaplanov <akaplanov@vyulabs.com>
2021-01-21 18:45:22 +0500

    Finished SequencedCollection

--- distribution/src/main/scala/com/vyulabs/update/distribution/mongo/SequencedCollection.scala
+++ distribution/src/main/scala/com/vyulabs/update/distribution/mongo/SequencedCollection.scala
@@ -8,12 +8,17 @@ import org.bson.codecs.configuration.CodecRegistry
 import org.bson.conversions.Bson
 import org.bson.{BsonDateTime, BsonDocument, BsonDocumentReader, BsonDocumentWrapper}
 import org.mongodb.scala.bson.BsonInt64
+import org.slf4j.LoggerFactory
 
 import scala.concurrent.{ExecutionContext, Future}
 import scala.reflect.{ClassTag, classTag}
 
 class SequencedCollection[T: ClassTag](collection: Future[MongoDbCollection[BsonDocument]], sequenceCollection: Future[MongoDbCollection[SequenceDocument]])
                             (implicit system: ActorSystem, materializer: Materializer, executionContext: ExecutionContext, codecRegistry: CodecRegistry) {
+  implicit val log = LoggerFactory.getLogger(getClass)
+
+  private var modifyInProcess = Option.empty[Future[Int]]
+
   def insert(document: T): Future[Unit] = {
     insert(Seq(document))
   }
@@ -37,8 +42,7 @@ class SequencedCollection[T: ClassTag](collection: Future[MongoDbCollection[Bson
 
   def find(filters: Bson = new BsonDocument()): Future[Seq[T]] = {
     for {
-      collection <- collection
-      docs <- collection.find(Filters.and(filters, Filters.exists("_expireTime", false)))
+      docs <- findDocuments(filters)
     } yield {
       docs.map(doc => {
         val codec = codecRegistry.get(classTag[T].runtimeClass.asInstanceOf[Class[T]])
@@ -48,23 +52,76 @@ class SequencedCollection[T: ClassTag](collection: Future[MongoDbCollection[Bson
   }
 
   def update(filters: Bson, modify: T => T): Future[Int] = {
+    def process(): Future[Int] = {
+      for {
+        collection <- collection
+        docs <- findDocuments(filters)
+        result <- {
+          Future.sequence(docs.map { doc =>
+            for {
+              sequence <- getNextSequence(collection.getName(), 1)
+              _ <- collection.updateOne(Filters.eq("_id", doc.getInt64("_id")),
+                Updates.combine(Updates.set("_replacedBy", new BsonInt64(sequence)), Updates.set("_expireTime", new BsonDateTime(System.currentTimeMillis()))))
+              result <- {
+                val codec = codecRegistry.get(classTag[T].runtimeClass.asInstanceOf[Class[T]])
+                val document = codec.decode(new BsonDocumentReader(doc), DecoderContext.builder.build())
+                val newDocument = modify(document)
+                val newDoc = BsonDocumentWrapper.asBsonDocument(newDocument, codecRegistry)
+                newDoc.append("_id", new BsonInt64(sequence))
+                newDoc.append("_time", new BsonDateTime(System.currentTimeMillis()))
+                collection.insert(newDoc)
+              }
+              _ <- collection.updateOne(Filters.eq("_id", doc.getInt64("_id")), Updates.unset("_replacedBy"))
+            } yield result
+          }).map(_.size)
+        }
+      } yield result
+    }
+
+    def queueFuture(): Future[Int] = {
+      synchronized {
+        val future = modifyInProcess match {
+          case Some(currentProcess) if !currentProcess.isCompleted =>
+            currentProcess.transformWith(_ => process())
+          case _ =>
+            process()
+        }
+        modifyInProcess = Some(future)
+        future.andThen {
+          case _ =>
+            synchronized {
+              modifyInProcess match {
+                case Some(`future`) =>
+                  modifyInProcess = None
+                case _ =>
+              }
+            }
+        }
+        future
+      }
+    }
+
+    queueFuture()
+  }
+
+  def delete(filters: Bson = new BsonDocument()): Future[Unit] = {
     for {
       collection <- collection
-      documents <- find(filters)
-      result <- collection.updateMany(Filters.and(filters, Filters.exists("_expireTime", false)),
+      result <- collection.updateMany(Filters.and(filters,
+        Filters.or(Filters.exists("_expireTime", false))),
         Updates.set("_expireTime", new BsonDateTime(System.currentTimeMillis()))).map(_ => ())
-      result <- {
-        insert(documents.map(modify(_))).map(_ => documents.size)
-      }
     } yield result
   }
 
-  def delete(filters: Bson): Future[Unit] = {
+  private def findDocuments(filters: Bson = new BsonDocument()): Future[Seq[BsonDocument]] = {
     for {
       collection <- collection
-      result <- collection.updateMany(filters,
-        Updates.set("_expireTime", new BsonDateTime(System.currentTimeMillis()))).map(_ => ())
-    } yield result
+      docs <- collection.find(Filters.and(filters,
+        Filters.or(Filters.exists("_expireTime", false), Filters.exists("_replacedBy", true))))
+    } yield {
+      val notReplaced = docs.filter(!_.containsKey("_replacedBy")).map(_.get("_id").asInt64())
+      docs.filter(doc => { !doc.containsKey("_replacedBy") || !notReplaced.contains(doc.get("_replacedBy").asInt64()) })
+    }
   }
 
   private def getNextSequence(sequenceName: String, increment: Int = 1): Future[Long] = {
--- distribution/src/test/scala/com/vyulabs/update/distribution/mongo/SequencedCollectionTest.scala
+++ distribution/src/test/scala/com/vyulabs/update/distribution/mongo/SequencedCollectionTest.scala
@@ -12,7 +12,7 @@ import org.slf4j.LoggerFactory
 
 import java.util.concurrent.TimeUnit
 import scala.concurrent.duration.FiniteDuration
-import scala.concurrent.{Await, Awaitable, ExecutionContext}
+import scala.concurrent.{Await, Awaitable, ExecutionContext, Future}
 
 class SequencedCollectionTest extends FlatSpec with Matchers with BeforeAndAfterAll {
   behavior of "Sequenced Collection"
@@ -37,9 +37,23 @@ class SequencedCollectionTest extends FlatSpec with Matchers with BeforeAndAfter
   val test = new SequencedCollection[TestRecord](collection, sequenceCollection)
 
   it should "insert/modify/delete records" in {
-    result(test.insert(TestRecord("f1", "f2")))
-    result(test.update(Filters.eq("field1", "f1"), f => TestRecord("field1", f.field2)))
-    println(result(test.find()))
-    //test.delete()
+    result(test.insert(TestRecord("v1", "v2")))
+    result(test.update(Filters.eq("field1", "v1"), { f => TestRecord("v3", f.field2) }))
+    assertResult(Seq(TestRecord("v3", "v2")))(result(test.find()))
+
+    var concurrentUpdate: Future[Int] = null
+    assertResult(1)(result(test.update(Filters.eq("field1", "v3"),
+      record => {
+        assertResult(Seq(TestRecord("v3", "v2")))(result(test.find()))
+        concurrentUpdate = test.update(Filters.eq("field1", "v3"), r => TestRecord(r.field1, "v5"))
+        TestRecord(record.field1, "v4")
+      }
+    )))
+    val res = result(concurrentUpdate)
+    assertResult(1)(res)
+    assertResult(Seq(TestRecord("v3", "v5")))(result(test.find()))
+
+    assertResult()(result(test.delete()))
+    assertResult(Seq.empty)(result(test.find()))
   }
 }
