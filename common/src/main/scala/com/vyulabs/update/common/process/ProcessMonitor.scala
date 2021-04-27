cf68efa1c3bf5ef84544a5a457bd589a85459d50
Andrew Kaplanov <akaplanov@gmail.com>
2021-04-27 21:38:29 +0500

    Terminate process group when max CPU percents reached

--- common/src/main/scala/com/vyulabs/update/common/process/ProcessMonitor.scala
+++ common/src/main/scala/com/vyulabs/update/common/process/ProcessMonitor.scala
@@ -5,6 +5,7 @@ import com.vyulabs.update.common.config.RestartConditions
 import org.slf4j.Logger
 
 import java.io.{File, IOException}
+import java.util.IllegalFormatException
 import java.util.concurrent.TimeUnit
 import scala.concurrent.duration.FiniteDuration
 
@@ -51,7 +52,7 @@ class ProcessMonitor(process: ChildProcess, conditions: RestartConditions)
             val percents = (cpuTime - lastCpuTime)*100/conditions.checkTimeoutMs
             println(s"${percents}%")
             if (percents >= maxCpuPercents) {
-              log.error(s"Process utilize ${percents}% of CPU time. That is more that maximum ${maxCpuPercents}%. Kill process group.")
+              log.error(s"Process utilize ${percents}% of CPU time. That is >= maximum ${maxCpuPercents}%. Kill process group.")
               stop()
               killProcessGroup()
             }
@@ -93,12 +94,63 @@ class ProcessMonitor(process: ChildProcess, conditions: RestartConditions)
   }
 
   private def getProcessCPU(handle: ProcessHandle): Option[Long] = {
-    val duration = handle.info().totalCpuDuration()
-    if (duration.isPresent) {
-      Some(duration.get().toMillis)
+    if (isUnix) {
+      try {
+        val proc = Runtime.getRuntime.exec(s"ps -o time= -p ${handle.pid()}", null, new File("."))
+        val stdOutput = ProcessUtils.readOutputToString(proc.getInputStream, ProcessUtils.Logging.None).getOrElse {
+          log.error(s"Get 'ps' output error")
+          return None
+        }
+        try {
+          val value = stdOutput.trim
+          if (!value.isEmpty) {
+            // Parse format [DD-]hh:mm:ss[.ms]
+            val index1 = value.lastIndexOf('.')
+            val millis = if (index1 != -1) value.substring(index1+1).toInt*10 else 0
+            val index2 = value.lastIndexOf(':', if (index1 != -1) index1-1 else value.length-1)
+            if (index2 == -1) {
+              throw new IOException()
+            }
+            val seconds = value.substring(index2+1, if (index1 != -1) index1 else value.length).toInt
+            val index3 = value.lastIndexOf(':', index2-1)
+            val minutes = value.substring(index3+1, index2).toInt
+            var hours, days = 0
+            if (index3 != -1) {
+              val index4 = value.lastIndexOf('-', index3-1)
+              hours = value.substring(index4+1, index3).toInt
+              if (index4 != -1) {
+                days = value.substring(0, index4).toInt
+              }
+            }
+            val time = FiniteDuration(days, TimeUnit.DAYS).toMillis +
+              FiniteDuration(hours, TimeUnit.HOURS).toMillis +
+              FiniteDuration(minutes, TimeUnit.MINUTES).toMillis +
+              FiniteDuration(seconds, TimeUnit.SECONDS).toMillis +
+              millis
+            Some(time)
+          } else {
+            None
+          }
+        } catch {
+          case ex: Exception =>
+            log.error(s"Parse 'ps' output error", ex)
+            None
+        }
+      } catch {
+        case ex: IOException =>
+          log.error("Process creation error", ex)
+          None
+      }
     } else {
       None
     }
+
+//    val duration = handle.info().totalCpuDuration()
+//    if (duration.isPresent) {
+//      Some(duration.get().toMillis)
+//    } else {
+//      None
+//    }
   }
 
   private def killProcessGroup(): Unit = {
--- common/src/test/scala/com/vyulabs/update/process/ProcessMonitorTest.scala
+++ common/src/test/scala/com/vyulabs/update/process/ProcessMonitorTest.scala
@@ -40,7 +40,7 @@ class ProcessMonitorTest extends FlatSpec with Matchers {
 
     val process = result(ChildProcess.start("/bin/sh", Seq(script.toString)))
     process.readOutput(onOutput = lines => lines.foreach { case (line, nl) => println(line) })
-    new ProcessMonitor(process, RestartConditions(None, Some(50), false, 100)).start()
+    new ProcessMonitor(process, RestartConditions(None, Some(100), false, 100)).start()
     result(process.onTermination())
   }
 }
