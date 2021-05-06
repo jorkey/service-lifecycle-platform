2f9989a9bfc6bd9bacdd90f833a754f66caa3e5f
Andrew Kaplanov <akaplanov@gmail.com>
2021-05-06 14:41:54 +0500

    .

--- dashboard/src/views/Profiles/components/Profiles/ServicesTable.tsx
+++ dashboard/src/views/Profiles/components/Profiles/ServicesTable.tsx
@@ -1,6 +1,9 @@
 import React, {useState} from "react";
 import {makeStyles} from "@material-ui/core/styles";
-import {DataGrid, GridColDef} from '@material-ui/data-grid';
+import {DataGrid, GridCellParams, GridColDef, GridRowId} from '@material-ui/data-grid';
+import {Pagination} from "@material-ui/lab";
+import {Button, IconButton} from "@material-ui/core";
+import DeleteIcon from '@material-ui/icons/Delete';
 
 const useStyles = makeStyles(theme => ({
   servicesTable: {
@@ -37,12 +40,43 @@ export const ServicesTable = (props: ServicesTableParams) => {
   const classes = useStyles();
 
   const columns: GridColDef[] = [
-    { field: 'service', headerName: 'Service', flex: 200 } ]
+    {
+      field: 'service',
+      headerName: 'Service',
+      flex: 200,
+      disableClickEventBubbling: true,
+      editable: true
+    },
+    {
+      field: 'actions',
+      headerName: 'Actions',
+      disableClickEventBubbling: true,
+      renderCell: (params: GridCellParams) => (
+        <strong>
+          <IconButton title='Delete' onClick={() => {}}>
+            <DeleteIcon/>
+          </IconButton>
+        </strong>
+      ),
+    }]
 
   const rows = getServices?.().map ((service, index) => {
     return { id: index, service: service }; }
   )
 
+  const [selectionModel, setSelectionModel] = React.useState<GridRowId[]>([]);
+
   return (
-    <DataGrid rows={rows} columns={columns} className={classes.servicesTable}/>)
+    <DataGrid rows={rows} columns={columns}
+              className={classes.servicesTable}
+              disableSelectionOnClick={true}
+              disableColumnSelector={true}
+              disableDensitySelector={true}
+              disableMultipleSelection={true}
+              onSelectionModelChange={(newSelection) => {
+                setSelectionModel([]);
+              }}
+              selectionModel={selectionModel}
+              hideFooter={true}
+    />)
 }
