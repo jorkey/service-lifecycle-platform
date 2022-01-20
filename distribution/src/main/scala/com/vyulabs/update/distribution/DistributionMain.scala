1f41d9926b961bee8015b7bfbe23761038b2b48c
Andrei Kaplanov <akaplanov@vyulabs.com>
2022-01-20 13:41:48 +0500

    Disable TLSv1.0 and TLSv1.1

--- distribution/src/main/scala/com/vyulabs/update/distribution/DistributionMain.scala
+++ distribution/src/main/scala/com/vyulabs/update/distribution/DistributionMain.scala
@@ -101,11 +101,11 @@ object DistributionMain extends App {
 
     val sslContext = SSLContext.getInstance("TLS")
     sslContext.init(keyManagerFactory.getKeyManagers, trustManagerFactory.getTrustManagers, new SecureRandom)
-    val protocols = sslContext.getSupportedSSLParameters.getProtocols
-    log.info(s"Original supported SSL protocols: ${protocols.toSeq}")
+    val protocols = sslContext.getDefaultSSLParameters.getProtocols
+    log.info(s"Original SSL protocols: ${protocols.toSeq}")
     val enabledProtocols = protocols.filter(_ != "TLSv1").filter(_ != "TLSv1.1")
     if (enabledProtocols.length != protocols.length) {
-      log.info(s"Set supported SSL protocols: ${enabledProtocols.toSeq}")
+      log.info(s"Set SSL protocols: ${enabledProtocols.toSeq}")
       sslContext.getSupportedSSLParameters.setProtocols(enabledProtocols)
     }
     ConnectionContext.httpsServer(sslContext)
